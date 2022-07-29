0a996183e7efb557352aac0e922d3f45664c9690
kgicheha <55036976+kgicheha@users.noreply.github.com>
2022-07-29 16:35:01 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Home from './components/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          RECIPE FINDER
+        </h1>
+      </header>
+      <h3>Dope Project Coming Soon! Stay Tuned</h3>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
