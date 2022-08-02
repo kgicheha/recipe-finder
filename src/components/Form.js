c393b9fab3057e018aeccfcba1ece1b0be663b58
kgicheha <55036976+kgicheha@users.noreply.github.com>
2022-08-01 18:50:44 -0700

    Add files via upload

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,20 +2,20 @@ import React, {useState} from 'react';
 
 function Form({ onSubmit }) {
   const [formData, setFormData] = useState({
-    ingredient1: "", 
-    ingredient2: "", 
-    ingredient3: "", 
-    ingredient4: "", 
+    ingredient1: "",
+    ingredient2: "",
+    ingredient3: "",
+    ingredient4: "",
     ingredient5: "",
   })
-  
-  function handleChange(event) { 
-    setFormData({...formData, 
+
+  function handleChange(event) {
+    setFormData({...formData,
       [event.target.name]: event.target.value
     })
   }
 
-  function handleSubmit(event) { 
+  function handleSubmit(event) {
     event.preventDefault();
     onSubmit(formData);
   }
@@ -23,11 +23,11 @@ function Form({ onSubmit }) {
       <form onSubmit={handleSubmit}>
           <label>Recipe:</label><br></br>
           <input type="text" id="name" name="ingredient1" value={formData.ingredient1} onChange={handleChange}/><br></br>
-          <input type="text" id="name" name="ingredient2" value={formData.ingredient2} onChange={handleChange}/><br></br> 
+          <input type="text" id="name" name="ingredient2" value={formData.ingredient2} onChange={handleChange}/><br></br>
           <input type="text" id="name" name="ingredient3" value={formData.ingredient3} onChange={handleChange}/><br></br>
-          <input type="text" id="name" name="ingredient4" value={formData.ingredient4} onChange={handleChange}/><br></br> 
+          <input type="text" id="name" name="ingredient4" value={formData.ingredient4} onChange={handleChange}/><br></br>
           <input type="text" id="name" name="ingredient5" value={formData.ingredient5} onChange={handleChange}/><br></br>
-          <input type="submit" value="Submit"/>
+          <input type="submit" value="Search"/>
       </form>
 )
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import Form from './Form'
-import RecipesContainer from './RecipesContainer'; 
+import RecipesContainer from './RecipesContainer';
 import recipesData from '../data/db.json';
 
-const Home = () => {
-  
-  
-  const [searchWords, setSearchWords] = useState(""); 
-  //const [recipes, setRecipes] = useState([]); 
+
+const Home = ({apiKey}) => {
+
+  const [searchWords, setSearchWords] = useState("");
+  // const [recipes, setRecipes] = useState([])
 
   const recipes = recipesData;
 
-  function onSubmit(formData) { 
+
+  function onSubmit(formData) {
     const values = Object.values(formData);
     const valuesFiltered = values.filter((value) => value !== "");
-    const valuesTrimmed = valuesFiltered.map((value) => value.trim()); 
+    const valuesTrimmed = valuesFiltered.map((value) => value.trim());
     const valuesString = valuesTrimmed.join(",+");
     console.log(valuesString);
     setSearchWords(valuesString);
   }
 
   // useEffect(() => {
-  //   fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${searchWords}&number=2`)
-  //   .then(res => res.json())
-  //   .then((data) => setRecipes(data))
+
+  //   fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=apples,+flour,+sugar&number=2`)
   // }, [searchWords]);
 
   return (
-    <div>Home
-      <Form 
+    <div>
+      <Form
         onSubmit={onSubmit}
       />
       <RecipesContainer
@@ -37,5 +37,4 @@ const Home = () => {
      </div>
   )
 }
-
 export default Home
