9d072b4fbb21bf46462ef06fedc738207c78ad9f
Tommy Nguyen <sixty22tommy@gmail.com>
2022-08-03 08:45:18 -0700

    working on details

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-function RecipeDetails({usedIngredientCount,usedIngredients, missedIngredientCount, missedIngredients}) {
+function RecipeDetails({usedIngredientCount, usedIngredients, missedIngredientCount, missedIngredients}) {
     const [inList, setInList] = useState(false);
     const [showDetails, setShowDetails] = useState(false)
 
@@ -8,28 +8,29 @@ function RecipeDetails({usedIngredientCount,usedIngredients, missedIngredientCou
         setInList((inList) => inList = !inList);
       }
 
-      //show Recipe details onClick
       const handleDetailsOnClick = () => {
         setShowDetails((showDetails) => showDetails = !showDetails )
       }
+
   return (
-    <>
-    <button onClick={handleDetailsOnClick}>Ingredients</button>
-    {showDetails ?
-    <>
-    <p>Ingredients: Have {usedIngredientCount} Missing: {missedIngredientCount}</p>
-        <ul>{usedIngredients.map((usedIngredient) => (
-          <ul key={usedIngredient.id}>{usedIngredient.name}
-          </ul>))}{missedIngredients.map((missedIngredient) => (
-          <ul>
-            <span>{missedIngredient.name}</span>
-            <button className="add" onClick={handleList}>{inList ? "Remove From List" : "Add To List"}</button>
-          </ul>
-        ))}</ul>
-    </>
-    : null
-    }
-    </>
+    <div>
+        <button onClick={handleDetailsOnClick}>Ingredients</button>
+        {showDetails ?
+      <div>
+      <p>Ingredients: Currently {usedIngredientCount} Missing Ingredients: {missedIngredientCount}</p>
+            <ul>{usedIngredients.map((usedIngredient) => (
+                <ul key={usedIngredient.id}>{usedIngredient.name}
+                </ul>))}{missedIngredients.map((missedIngredient) => (
+                <ul>
+                  <span>{missedIngredient.name}</span>
+                  <button className="add" onClick={handleList}>{inList ? "Remove From List" : "Add To List"}</button>
+                </ul>
+        ))}
+            </ul>
+      </div>
+        : null
+        }
+    </div>
   )
 }
 
