7c8d7beb15643b7d7d90a82c221f3f7abe7d9870
Kevin Gicheha <gichehakevin@gmail.com>
2022-08-03 20:42:50 -0700

    added functionality to fetch and delete

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,26 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import RecipeDetails from './RecipeDetails'
 
-function Recipe({ recipe, addToFavoritesList, addToShoppingList  }) {
+function Recipe({ recipe, addToFavoritesList, addToShoppingList, removeFromFavoritesList  }) {
   const {id, title, image, usedIngredientCount, usedIngredients, missedIngredientCount, missedIngredients } = recipe;
   const [isFavorite, setIsFavorite] = useState(false);
 
   function onFavoriteClick() {
     //CREATE POST ELEMENT
     setIsFavorite((isFavorite) => (!isFavorite));
+    }
 
-    fetch(`http://localhost:6001/favorites`,{
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify(recipe)
-    })
-      .then(resp => resp.json())
-      .then(addToFavoritesList(recipe))
-  }
-
+   useEffect(()=>{
+      if(isFavorite ===true) {
+        console.log("Its true")
+        fetch(`http://localhost:6001/favorites`,{
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          body: JSON.stringify(recipe)
+        })
+          .then(resp => resp.json())
+          .then(addToFavoritesList(recipe))
+        } else {
+          fetch(`http://localhost:6001/favorites/${id}`, {
+            method: "DELETE"
+          })
+          .then(resp =>resp.json())
+          .then(removeFromFavoritesList(id))
+          }
+        }, [isFavorite])
 
   useEffect(() => {
     console.log(isFavorite);
