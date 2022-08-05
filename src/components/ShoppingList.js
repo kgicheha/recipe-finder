2476c2d899d8c89af24d3b9fde324725401ed190
kgicheha <55036976+kgicheha@users.noreply.github.com>
2022-08-05 00:18:38 -0700

    Update ShoppingList.js

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid';
+import ShoppingListCard from './ShoppingListCard';
 
 function ShoppingList({ shoppingList }) {
-  const shoppingItems = shoppingList.map((item) => (
-    <h3 key={uuidv4()}>{item}</h3>
+  
+//   const shoppingItems = shoppingList.map((item) => (
+//     <h3 key={uuidv4()}>{item}</h3>
+//   ))
+  
+  
+  const ShoppingListItems = shoppingList.map((recipe) => (
+    <ShoppingListCard
+      recipe={recipe}
+    />
   ))
   return (
   <div className="shopContainer" id="shoppingItems">
